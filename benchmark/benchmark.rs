ca8039fc651aa122e7f959d3d62e681501679b76
Pascal Seitz <pascal.seitz@gmail.com>
2018-08-16 16:37:56 +0900

    cargo fmt

--- benchmark/benchmark.rs
+++ benchmark/benchmark.rs
@@ -3,22 +3,21 @@
 extern crate test;
 extern crate tinysegmenter;
 
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
 use test::Bencher;
 
 #[bench]
 fn run(b: &mut Bencher) {
-  // http://www.genpaku.org/timemachine/timemachineu8j.txt
-  let mut f =
-    File::open("benchmark/timemachineu8j.txt")
-    .expect("Failed to read a benchmark text.");
-  let mut s = String::new();
-  let _ = f.read_to_string(&mut s);
+    // http://www.genpaku.org/timemachine/timemachineu8j.txt
+    let mut f =
+        File::open("benchmark/timemachineu8j.txt").expect("Failed to read a benchmark text.");
+    let mut s = String::new();
+    let _ = f.read_to_string(&mut s);
 
-  b.iter(|| tinysegmenter::tokenize(&s));
+    b.iter(|| tinysegmenter::tokenize(&s));
 }
 #[bench]
 fn test_small(b: &mut test::Bencher) {
-  b.iter(|| tinysegmenter::tokenize("私はおでぶです"))
+    b.iter(|| tinysegmenter::tokenize("私はおでぶです"))
 }
--- test/test.rs
+++ test/test.rs
@@ -2,13 +2,28 @@ extern crate tinysegmenter;
 
 #[test]
 fn tokenize() {
-  assert_eq!(
-    tinysegmenter::tokenize("私の名前は中野です"),
-    ["私", "の", "名前", "は", "中野", "です"]);
+    assert_eq!(
+        tinysegmenter::tokenize("私の名前は中野です"),
+        ["私", "の", "名前", "は", "中野", "です"]
+    );
 
-  assert_eq!(
-    tinysegmenter::tokenize("TinySegmenterは25kBで書かれています。"),
-    ["TinySegmenter", "は", "2", "5", "kB", "で", "書か", "れ", "て", "い", "ます", "。"]);
+    assert_eq!(
+        tinysegmenter::tokenize("TinySegmenterは25kBで書かれています。"),
+        [
+            "TinySegmenter",
+            "は",
+            "2",
+            "5",
+            "kB",
+            "で",
+            "書か",
+            "れ",
+            "て",
+            "い",
+            "ます",
+            "。"
+        ]
+    );
 
-  assert_eq!(tinysegmenter::tokenize(""), [] as [&str; 0]);
+    assert_eq!(tinysegmenter::tokenize(""), [] as [&str; 0]);
 }
